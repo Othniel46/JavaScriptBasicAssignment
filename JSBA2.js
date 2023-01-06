7aef20e2ee712823d0a1b7f0c53eef5f58cbfc27
Jonathan  Sadullo WD42P <73685841+Othniel46@users.noreply.github.com>
2023-01-06 17:37:05 +0800

    Add files via upload

---
+++ JSBA2.js
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+    <html>
+        <head>
+            <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+            <meta name="viewport" width="device-width" initial-scale="1" maximum-scale="1">
+            <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/
+w3.css">
+        </head>
+        <body>
+         <script>
+         let fahrenheit = 77;
+ let result =  (5/9) * (fahrenheit-32);
+ console.log(result);
+          convert(result, 'fahrenheit').to('celsius'); 
+            </script>   
+        </body>
+    </html>
