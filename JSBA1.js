c9544509221101955c6fcadb1975f3633e373497
Jonathan  Sadullo WD42P <73685841+Othniel46@users.noreply.github.com>
2023-01-06 17:36:38 +0800

    Add files via upload

---
+++ JSBA1.js
@@ -0,0 +1,16 @@
+<DOCTYPE html>
+    
+    <html>
+        <head>
+            <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+            <meta name="viewport" width="device-width" initial-scale="1" maximum-scale="1">
+            <link rel="stylesheet" href="#">
+        </head>
+        <body>
+          <script>
+            let x = 4 * 3;   
+ console.log(12);
+
+           </script> 
+        </body>
+    </html>
